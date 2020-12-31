09d08392ec1be263aa161a4fc7ff35cd70b979fa
Rakesh Baravathaya Badaje <baravathaya@gmail.com>
2020-12-31 11:43:47 -0500

    CommonController Functionality Implemented

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -0,0 +1,32 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.UserDetailsResponse;
+import com.upgrad.quora.service.business.CommonService;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class CommonController {
+
+    @Autowired
+    private CommonService commonService;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/userprofile/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<UserDetailsResponse> getUserProfile(@PathVariable("id") final String uuid,
+                                                              @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, UserNotFoundException {
+        String [] bearerToken = authorization.split("Bearer ");
+        final UserEntity userEntity = commonService.getUserProf(uuid,bearerToken[1]);
+        UserDetailsResponse userDetailsResponse = new UserDetailsResponse().firstName(userEntity.getFirstName())
+                .lastName(userEntity.getLastName()).userName(userEntity.getUserName()).emailAddress(userEntity.getEmail())
+                .country(userEntity.getCountry()).aboutMe(userEntity.getAboutMe()).dob(userEntity.getDob()).contactNumber(userEntity.getContactNumber());
+        return new ResponseEntity<UserDetailsResponse>(userDetailsResponse, HttpStatus.OK);
+    }
+
+}
