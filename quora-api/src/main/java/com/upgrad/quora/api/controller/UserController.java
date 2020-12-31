295f22a129623d9a0e629b0664615cf872af7459
shrish.bajpai.in@gmail.com <shirish.bajpai.in@gmail.com>
2020-12-31 15:49:32 +0000

    added user controller

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -0,0 +1,111 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.SigninResponse;
+import com.upgrad.quora.api.model.SignoutResponse;
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
+//import com.upgrad.quora.service.business.AuthenticationService;
+//import com.upgrad.quora.service.business.SignoutService;
+import com.upgrad.quora.service.business.SignupBusinessService;
+//import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import com.upgrad.quora.service.exception.SignOutRestrictedException;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Base64;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @Autowired
+    private SignupBusinessService signupBusinessService;
+
+    @RequestMapping(method = RequestMethod.POST, path = "/user/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupUserResponse> userSignup(final SignupUserRequest signupUserRequest) throws SignUpRestrictedException {
+
+        final UserEntity userEntity = new UserEntity();
+
+        userEntity.setUuid(UUID.randomUUID().toString());
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setLastName(signupUserRequest.getLastName());
+        userEntity.setUserName(signupUserRequest.getUserName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setPassword(signupUserRequest.getPassword());
+        userEntity.setCountry(signupUserRequest.getCountry());
+        userEntity.setAboutMe(signupUserRequest.getAboutMe());
+        userEntity.setDob(signupUserRequest.getDob());
+        userEntity.setContactNumber(signupUserRequest.getContactNumber());
+        userEntity.setSalt("1234abc");
+        userEntity.setRole("nonadmin");
+
+        final UserEntity createdUserEntity = signupBusinessService.signup(userEntity);
+        SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
+        if(userResponse == null){
+            throw new SignUpRestrictedException("SGR-003", "Incomplete or incorrect data");
+        }
+        return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
+    }
+/*
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @RequestMapping(method = RequestMethod.POST, path = "/user/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SigninResponse> login(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
+        byte[] decode;
+        try {
+            decode = Base64.getDecoder().decode(authorization.split("Basic ")[1]);
+
+        }
+        catch (Exception authStringException){
+            throw new AuthenticationFailedException("ATH-003", "incorrect authorisation format");
+        }
+
+        String decodedText = new String(decode);
+        String[] decodedArray = decodedText.split(":");
+        //authentication service does authentication based on username and password. Authentication service in-turn calls UserDao to create auth token
+        UserAuthTokenEntity userAuthToken = authenticationService.authenticate(decodedArray[0], decodedArray[1]);
+
+        UserEntity user = userAuthToken.getUser();
+
+        SigninResponse authorizedUserResponse = new SigninResponse().id(user.getUuid());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access-token", userAuthToken.getAccessToken());
+        return new ResponseEntity<SigninResponse>(authorizedUserResponse, headers, HttpStatus.OK);
+
+
+
+    }
+
+    @Autowired
+    private SignoutService signoutService;
+    @RequestMapping(method = RequestMethod.POST, path = "/user/signout", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignoutResponse> logout(@RequestHeader("authorization") final String accessToken) throws SignOutRestrictedException {
+
+        //signout using access token
+        UserAuthTokenEntity userAuthToken = signoutService.signout(accessToken);
+
+        //return uuid of the user
+        SignoutResponse authorizedUserResponse = new SignoutResponse().id(userAuthToken.getUuid());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access-token", userAuthToken.getAccessToken());
+        return new ResponseEntity<SignoutResponse>(authorizedUserResponse, headers, HttpStatus.OK);
+    }
+
+*/
+}
+
+
