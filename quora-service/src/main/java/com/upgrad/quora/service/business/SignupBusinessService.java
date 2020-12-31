d8df84251ffef7396b0f5e17d5ce656de30d33f3
shrish.bajpai.in@gmail.com <shirish.bajpai.in@gmail.com>
2020-12-31 15:55:51 +0000

    added user signup busienss service and user auth token entity

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/SignupBusinessService.java
@@ -0,0 +1,40 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SignupBusinessService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private PasswordCryptographyProvider passwordCryptographyProvider;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserEntity signup(UserEntity userEntity) throws SignUpRestrictedException {
+
+        if (userDao.getUserByUserName(userEntity.getUserName()) != null) {
+            throw new SignUpRestrictedException ("SGR-001", "Try any other Username, this Username has already been taken");
+        }
+
+        if (userDao.getUserByEmail(userEntity.getEmail()) != null) {
+            throw new SignUpRestrictedException ("SGR-002", "This user has already been registered, try with any other emailId");
+        }
+        String[] encryptedText = passwordCryptographyProvider.encrypt(userEntity.getPassword());
+        userEntity.setSalt(encryptedText[0]);
+        userEntity.setPassword(encryptedText[1]);
+        UserEntity ue = userDao.createUser(userEntity);
+        if (ue == null){
+
+            throw new SignUpRestrictedException("SGR-003", "Incomplete signup data");
+        }
+        return ue;
+    }
+}
